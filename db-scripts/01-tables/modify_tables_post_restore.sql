72e75182689a9ae325ee489b21558bf7b02e77bc
jaya-venkatesan <jaya.venkatesan@atos.net>
2025-09-05 12:54:18 +0530

    feat: add comments

--- db-scripts/01-tables/modify_tables_post_restore.sql
+++ db-scripts/01-tables/modify_tables_post_restore.sql
@@ -1,8 +1,8 @@
--- Fix deprecated fetaures
+--- Fix deprecated fetaures
 
 ALTER TABLE [dbo].[Auditlog] ALTER COLUMN [Parameters] VARCHAR(MAX) NULL;
 
 ALTER TABLE [dbo].[AuditlogArchived] ALTER COLUMN [Parameters] VARCHAR(MAX) NULL;
 
--- Schema Fixes 
+--- Schema Fixes 
  ALTER TABLE dbo.tSites ADD CONSTRAINT PK_PlantNo PRIMARY KEY CLUSTERED (PlantNo);  -- Creating Primary Key for tSites table.
